a21a1e66925253d33f867c9b132e39f59cc2f6c6
Marcelo Freire <marcelofreire@sas.com.br>
2021-07-17 20:23:06 -0300

    Create new edition based from original game

--- interfaces.ts
+++ interfaces.ts
@@ -3,7 +3,7 @@ interface Game {
   description: string
   readonly genre: string
   platform?: string[]
-  getSimilars: (title: string) => void
+  getSimilars?: (title: string) => void
 }
 
 const ori: Game = {
@@ -20,4 +20,20 @@ console.log(ori.title)
 console.log(ori.genre)
 // ori.genre = 'New genre' // Cannot assign to 'genre' because it is a read-only property.ts(2540)
 
-ori.getSimilars(ori.title)
+if (ori.getSimilars) {
+  ori.getSimilars(ori.title)
+}
+
+interface DefinitiveEdition extends Game {
+  originalGame: Game
+  newContent: string[]
+}
+
+const newEdition: DefinitiveEdition = {
+  title: 'Ori and the Blind Forest: Definitive Edition',
+  description: 'This is the longest version of Ori and the Blind Forest',
+  genre: 'Metroidvania',
+  platform: ['Xbox', 'Windows', 'Switch'],
+  originalGame: ori,
+  newContent: ['New areas', 'New abilities', 'more story sequences', 'multiple difficult modes', 'Fast travel between areas']
+}
