7610a9a9a4a5c80768672f80f7cc6b48851b56a0
Marcelo Freire <marcelofreire@sas.com.br>
2021-07-17 19:52:14 -0300

    Add modifier readonly to genre

--- interfaces.ts
+++ interfaces.ts
@@ -1,7 +1,7 @@
 interface Game {
   title: string
   description: string
-  genre: string
+  readonly genre: string
   platform: string[]
   getSimilars: (title: string) => void
 }
@@ -17,5 +17,7 @@ const ori: Game = {
 }
 
 console.log(ori.title)
+console.log(ori.genre)
+// ori.genre = 'New genre' // Cannot assign to 'genre' because it is a read-only property.ts(2540)
 
 ori.getSimilars(ori.title)
