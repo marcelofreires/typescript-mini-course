11f4be04fb6eda2a32639c0d21dd007906730bd5
Marcelo Freire <marcelofreire@sas.com.br>
2021-07-18 00:36:18 -0300

    Apply generic on useState

--- generics.ts
+++ generics.ts
@@ -1,21 +1,27 @@
-function useState() {
-  let state: number | string
+// S => State
+// T => Type
+// K => Key
+// V => Value
+// E => Element
+
+function useState<S extends number | string>() {
+  let state: S
 
   function getState() {
     return state
   }
 
-  function setState(newState: number | string) {
+  function setState(newState: S) {
     state = newState
   }
 
   return { getState, setState }
 }
 
-const newState = useState()
+const newState = useState<string>()
 
-newState.setState(123)
-console.log(newState.getState())
+// newState.setState(123) // Argument of type 'number' is not assignable to parameter of type 'string'.ts(2345)
+// console.log(newState.getState())
 
 newState.setState('foo')
 console.log(newState.getState())
