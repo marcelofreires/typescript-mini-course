56368ea31cad7ce86101693649e18d9911127778
Marcelo Freire <marcelofreire@sas.com.br>
2021-07-19 00:12:54 -0300

    Add a property decorator example

--- decorators.ts
+++ decorators.ts
@@ -17,6 +17,39 @@ class API {}
 console.log('API =>', new API())
 
 // Property decorator
+function minLength(length: number) {
+  return (target: any, key: string) => {
+    let val = target[key]
+
+    const getter = () => val
+
+    const setter = (value: string) => {
+      if (value.length < length) {
+        console.log(`Error: you can't create ${key} with less than ${length} characters`)
+      } else {
+        val = value
+      }
+    }
+
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+    })
+  }
+}
+
+class Movie {
+  @minLength(50)
+  title: string
+
+  constructor(title: string) {
+    this.title = title
+  }
+}
+
+const movie = new Movie('Interstellar')
+// console.log(movie)
+
 // Method decorator
 // Parameter decorator
 // Acessor decorator
