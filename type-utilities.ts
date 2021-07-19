b78e171e53605b529168be04123e1e7c0feaebb4
Marcelo Freire <marcelofreire@sas.com.br>
2021-07-18 22:07:31 -0300

    Add Partial to allow add a new property value

---
+++ type-utilities.ts
@@ -0,0 +1,22 @@
+type Todo = {
+  title: string
+  description: string
+  completed: boolean
+}
+
+const todo: Readonly<Todo> = {
+  title: 'Ler Fundação de Isaac Asimov',
+  description: 'Começar a série de livros Fundação',
+  completed: false,
+}
+
+function updateTodo(todo: Todo, fieldsToUpadate: Partial<Todo>) {
+  return { ...todo, ...fieldsToUpadate }
+}
+
+const newTodo: Todo = updateTodo(todo, { completed: true })
+
+console.log('todo', todo)
+
+console.log('newTodo', newTodo)
+
