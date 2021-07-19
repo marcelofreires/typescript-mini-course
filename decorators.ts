81a019399cc0b8087f5a7fb8f157119ad763f318
Marcelo Freire <marcelofreire@sas.com.br>
2021-07-18 23:26:11 -0300

    Create the first decorator

---
+++ decorators.ts
@@ -0,0 +1,16 @@
+// @Component
+// @Selector
+// @useState("qwert")
+
+function log(target) {
+  console.log(target)
+}
+
+@log
+class Foo {}
+
+// Class decorator
+// Property decorator
+// Method decorator
+// Parameter decorator
+// Acessor decorator
